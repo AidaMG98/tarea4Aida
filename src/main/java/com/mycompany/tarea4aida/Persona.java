a7e988c952a463f56aee75df11a2695f27584ec9
AidaMG98 <aida3montes@gmail.com>
2020-04-23 17:44:17 +0200

    Añade constructores en clase modelo

--- src/main/java/com/mycompany/tarea4aida/Persona.java
+++ src/main/java/com/mycompany/tarea4aida/Persona.java
@@ -13,4 +13,14 @@ public class Persona {
     String nombre;
     String apellidos;
     int edad;
+
+    public Persona(String nombre, String apellidos, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+    
+    public Persona(){
+    
+    }
 }
